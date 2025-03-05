feecd21b1a39bf1259c9e09c317e7df4a642e1e6
Andrea Aceto <andrea.aceto20@gmail.com>
2025-03-05 20:22:09 +0100

    [FIX]overviews cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,10 +4,10 @@ export default function Card({ props, url }) {
             <div className='card' key={props.id}>
                 <img src={url + props.poster_path} alt="" className="hover" />
                 <div className="pop">
-                    <h2 className="pop">Titolo: {props.title}</h2>
-                    <p className="pop ">Titolo originale: {props.original_title}</p>
-                    <p className="pop ">Voto: {Math.ceil(Math.ceil(props.vote_average) / 2)}</p>
-                    <p className="pop ">Overview: {props.overview}</p>
+                    <h2 >Titolo: {props.title}</h2>
+                    <p >Titolo originale: {props.original_title}</p>
+                    <p >Voto: {Math.ceil(Math.ceil(props.vote_average) / 2)}</p>
+                    <p>Overview: {props.overview}</p>
                     {/* <FontAwesomeIcon icon={faStar} /> */}
                 </div>
             </div >
--- src/components/CardSerie.jsx
+++ src/components/CardSerie.jsx
@@ -3,10 +3,10 @@ export default function CardSerie({ props, url }) {
         <>
             <div className='card' key={props.id}>
                 <img src={url + props.poster_path} alt="" />
-                <h2 className="pop title">Titolo: {props.name}</h2>
-                <p className="pop original_title">Titolo originale: {props.original_name}</p>
-                <p className="pop vote">Voto: {Math.ceil(Math.ceil(props.vote_average) / 2)}</p>
-                <p className="pop overview">Overview: {props.overview}</p>
+                <h2 >Titolo: {props.name}</h2>
+                <p >Titolo originale: {props.original_name}</p>
+                <p >Voto: {Math.ceil(Math.ceil(props.vote_average) / 2)}</p>
+                <p >Overview: {props.overview}</p>
                 {/* <FontAwesomeIcon icon={faStar} /> */}
             </div>
         </>
