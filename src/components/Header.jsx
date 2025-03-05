5b15a8a95b434d37e3273d0b21030d77967b01d5
Andrea Aceto <andrea.aceto20@gmail.com>
2025-03-05 17:49:59 +0100

    [ADD]Milestone 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,7 @@ const initialFormData = {
 export default function Header() {
     const [movies, setMovies] = useState([])
     const [search, setSearch] = useState(initialFormData)
+    const [tv, setTv] = useState([])
     function fetchMovies() {
         const options = {
             method: 'GET',
@@ -24,7 +25,7 @@ export default function Header() {
             .request(options)
             .then((res) => {
                 setMovies(res.data.results)
-                console.log(res.data)
+                // console.log(res.data)
             })
             .catch(err => console.error(err));
     }
@@ -41,6 +42,28 @@ export default function Header() {
         e.preventDefault();
     }
 
+    function fetchTV() {
+        const option = {
+            method: 'GET',
+            url: 'https://api.themoviedb.org/3/search/tv',
+            params: { query: search.title, include_adult: 'false', language: 'en-US', page: '1' },
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MWNiMDg3N2I3ODljMDY5YjkyNGZiZjI1Zjc5NjZiMCIsIm5iZiI6MTc0MTE4NzU5MC40NTksInN1YiI6IjY3Yzg2YTA2ODMwNTcwZDFmZGMyYWFhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oaOQXMwIREsypj2NngV5Q0J7Ggfp_BbkbM0zhrqQfAY'
+            }
+        };
+
+        axios
+            .request(option)
+            .then(res => setTv(res.data.results))
+            .catch(err => console.error(err));
+    }
+
+    function fetchAll() {
+        fetchMovies();
+        fetchTV();
+    }
+
     useEffect(fetchMovies, [])
     return (
         <>
@@ -53,10 +76,11 @@ export default function Header() {
                             value={search.title}
                             placeholder="search"
                             onChange={handleFormData} />
-                        <button onClick={fetchMovies}>Search</button>
+                        <button onClick={fetchAll}>Search</button>
                     </form>
                 </nav>
             </header>
+            <h1>film</h1>
             {
                 movies.map((movie) => (
 
@@ -65,6 +89,19 @@ export default function Header() {
                         <p>{movie.original_title}</p>
                         <p>{search.language}</p>
                         <p>{movie.vote_avarage}</p>
+                    </div>
+
+                ))
+            }
+            <h1>serie tv</h1>
+            {
+                tv.map((serie) => (
+
+                    <div className='card' key={serie.id}>
+                        <h2>{serie.name}</h2>
+                        <p>{serie.original_name}</p>
+                        <p>{search.language}</p>
+                        <p>{serie.vote_avarage}</p>
                     </div>
 
                 ))
