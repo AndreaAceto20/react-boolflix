3ecee64a2cf4640391512e6a7d9469d88de08be7
Andrea Aceto <andrea.aceto20@gmail.com>
2025-03-05 20:03:01 +0100

    [FIX]Backgroundcolor

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,13 @@ export default function Card({ props, url }) {
         <>
             <div className='card' key={props.id}>
                 <img src={url + props.poster_path} alt="" className="hover" />
-                <h2 className="pop title">Titolo: {props.title}</h2>
-                <p className="pop original_title">Titolo originale: {props.original_title}</p>
-                <p className="pop vote">Voto: {Math.ceil(Math.ceil(props.vote_average) / 2)}</p>
-                <p className="pop overview">Overview: {props.overview}</p>
-                {/* <FontAwesomeIcon icon={faStar} /> */}
+                <div className="pop">
+                    <h2 className="pop">Titolo: {props.title}</h2>
+                    <p className="pop ">Titolo originale: {props.original_title}</p>
+                    <p className="pop ">Voto: {Math.ceil(Math.ceil(props.vote_average) / 2)}</p>
+                    <p className="pop ">Overview: {props.overview}</p>
+                    {/* <FontAwesomeIcon icon={faStar} /> */}
+                </div>
             </div >
         </>
     )
